35e16e7333c6b7a5c239f0fcb0db9f6f58559c1b
Sarahaime Rodriguez Torres <sarahaime.rodriguez.torres@infox.de>
2024-12-10 19:11:17 +0100

    project structure added

---
+++ generator_app/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home(request: HttpRequest):
+    return HttpResponse( 'Hello world' )
